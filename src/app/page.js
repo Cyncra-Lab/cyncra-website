dd510959c5101f8e31b3f4719bf47dd7bb7e5632
Ekeh Pearl <pearlekeh7@gmail>
2025-07-14 10:45:11 +0100

    feat(choose-section): add “Why Choose Cyncra” full-width section
    - Created `ChooseSection.js`- Added placeholder TILES data with four core value props
    - Ready to drop into homepage after Services section

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,19 @@
 import AboutSection from "./components/AboutSection";
+import ChooseSection from "./components/ChooseSection";
 import HeroSection from "./components/HeroSection";
 import ServicesSection from "./components/ServiceSection";
 import StatsSection from "./components/StatsSection";
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
-      <HeroSection />
-      <StatsSection />
-      <AboutSection />
-      <ServicesSection />
-    </div>
+    <>
+      <div className="container mx-auto">
+        <HeroSection />
+        <StatsSection />
+        <AboutSection />
+        <ServicesSection />
+      </div>
+      <ChooseSection />
+    </>
   );
 }
