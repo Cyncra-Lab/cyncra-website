d8c128c5f73f60d7b128ac065a3bb83843ec37be
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-07-15 05:59:37 +0100

    (feat): Build mission section

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,11 @@
 import Hero from './components/Hero';
+import Mission from './components/Mission';
 
 export default function AboutPage() {
     return (
         <>
             <Hero />
+            <Mission />
         </>
     )
 }
