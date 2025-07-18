89b11868b169d3c02adb68550b62ca1f92e0cd00
Racheal Frontend developer <destinifeoluwayemisi@gmail.com>
2025-07-18 14:31:40 +0100

    Added SEO Data

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,10 +4,17 @@ import ProjectCTA from '../components/Project';
 import MapSection from '../components/MapSection';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer'
+import Head from 'next/head';
 
 export default function ContactPage() {
     return(
         <>
+        <Head> 
+            <title>
+                Cyncra Technologies -- Contact Us for Expert  Tech Support & Services
+            </title>
+            <meta name="description" content="Connect with Cyncra Technologies for product design, development, and digital strategy support. We're here to answer your questions and bring your ideas to life " />
+        </Head>
         <ContactHero/>
         <ContactInfoForm/>
         <ProjectCTA/>
--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -3,16 +3,22 @@ import ServiceGrid from '../components/ServiceGrid';
 import HowItWorks from '../components/ServiceHowItWorks';
 import ProjectCTA from '../components/serviceProject';
 import ContactInfoForm from '../components/SeerviceInfoForm';
-import Footer  from '../components/Footer';
+import Head from 'next/head';
 
 export default function ServicePage(){
     return (
-        <main>
+        <>
+                <Head> 
+                    <title>
+                        Cyncra Technologies -- Custom Web, App & Digital Solutions for Buisness Growth
+                    </title>
+                    <meta name="description" content="Explore our expert services-from website and mobile app development to UI/UX design, eCommerce, and digital strategy Cyncra builds scalable digital products that deliver results." />
+                </Head>
             <ServiceHero/>
             <ServiceGrid/>
             <HowItWorks/>
             <ProjectCTA/>
             <ContactInfoForm/>
-        </main>
+        </>
     );
 }
